c7ab6a9b636a3df162fe61920beecadc209e9bf6
Wirakarn <wirakarn.ang@gmail.com>
2026-07-06 16:11:58 +0700

    fist MongoDB commit

---
+++ query2.mongodb.js
@@ -0,0 +1,13 @@
+
+// Exercise 3
+
+use("sample_mflix");
+
+// How many theaters does AL state has?
+// db.theaters.find({ "location.address.state": "AL" }).count()
+
+// How many theaters does La Quinta city has?
+// db.theaters.find({ "location.address.city": "La Quinta" }).count()
+
+// What is an example of each documents of above like?
+db.theaters.findOne({ "location.address.state": "AL"})
